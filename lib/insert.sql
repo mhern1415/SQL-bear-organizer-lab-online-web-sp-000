b310d237c48cb9c4278acf7da8f01fffdbe5ab53
Mark Hernandez <m.hern1415@gmail.com>
2019-08-20 16:31:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,5 @@
-INSERT INTO bears 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3, "male", "brown", "mean", )
+
+
+
+INSERT INTO cats (name, age, breed, owner_id) VALUES ("Maru", 3 , "Scottish Fold", 1);
